bb5544303a897a51a976edff24491b7e6b0fc47a
Joacim Delfin Sveen <jdelfin.sveen@outlook.com>
2020-10-18 16:57:57 +0200

    Changed the main file to be using PRAW. Also updated README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Joke of the day
-Gets the top joke on /r/jokes every 24 hours.
+Joke of the day is a python script that gets the top joke on /r/jokes every 24 hours. 
+
+## Installation
+
+You will need [PRAW: The Python Reddit API Wrapper](https://praw.readthedocs.io/en/latest/index.html#)
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install praw.
+
+```bash
+pip install praw
+```
+
+## Usage
+
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-import requests
-base_url = 'https://www.reddit.com/'
-data = {'grant_type': 'password',
-        'username': 'deCastillon', 'password': 'TCF6GAfzs58c'}
-auth = requests.auth.HTTPBasicAuth(
-    'tRLX1U8HSRXogA', 'v-7bizF5h9_sWCtRe6X8rhkB2A4')
-# A token created using username, password, app-id and app-secret.
-r = requests.post(base_url + 'api/v1/access_token', data=data,
-                  headers={'user-agent': 'joke-of-the-day by deCastillon'}, auth=auth)
-d = r.json()
+import praw
 
-# API requests should be done to https://oauth.reddit.com
-token = 'bearer' + d['access_token']
-base_url = 'https://oauth.reddit.com'
+reddit = praw.Reddit(
+    client_id = "tRLX1U8HSRXogA",
+    client_secret = "v-7bizF5h9_sWCtRe6X8rhkB2A4",
+    user_agent = "joke-of-the-day:v.1 (by /u/deCastillon)"
+)
 
-# Authentication token
-headers = {'Authorization': token,
-           'User-Agent': 'joke-of-the-day by deCastillon'}
-response = requests.get(base_url + '/api/v1/me', headers=headers)
-
-if response.status_code == 200:
-    print(response.json()['name'], response.json()['comment_karma'])
-
-print(r.status_code)
+print(reddit.read_only)
